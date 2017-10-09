515263ca41fba9533dcef7dcc8eab718a016d45c
flotux <flvitalli@gmail.com>
2017-10-09 22:19:54 +0200

    add file

---
+++ view/popUp.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# Author : florian Vitalli
+# Contact : flvitalli@gmail.com
+
+"""
+    popUp.py
+
+    Contain the pop up pages.
+
+        -- LoadPage -- The page to find and load programmes.
+        -- NewPage -- The page to create a new programme.
+        """
+
+from tkinter import *
+
+
+class LoadPage(Tk):
+    """ Find and load programmes. """
+    pass
+
+
+class NewPage(Tk):
+    """ Create a new programme. """
+    pass
+
+
+class EntryConf(Tk):
+    """ Configure Entry. """
+    pass
+
+
+class SensorConf(Tk):
+    """ Configure Sensors. """
+    pass
+
+
+class CalcSpr(Tk):
+    """ Calculate values for springs. """
+    pass
+
+
+class CalcSpin(Tk):
+    """ Calculate values for spinners. """
+    pass
+
+
+class CalcArc(Tk):
+    """ Calculate values for arc feed. """
+    pass
