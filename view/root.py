1ffa48c01e1c1da7c0eb922c30a140b1b5f33730
flotux <flvitalli@gmail.com>
2017-10-09 22:19:47 +0200

    add file

---
+++ view/root.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# Author : florian Vitalli
+# Contact : flvitalli@gmail.com
+
+"""
+    Root.py
+
+    A collection of the principal windows on the programme.
+
+        -- StartMenu -- The menu at the start of the programme.
+        -- OverView -- The page how the principal informations have been print.
+        """
+
+from tkinter import *
+
+class StartMenu(Tk):
+    """ The menu at the start of the programme. """
+    pass
+
+
+class OverView(Tk):
+    """ The page how the principal informations have been print. """
+    pass
