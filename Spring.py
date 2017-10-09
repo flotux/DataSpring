9ec7bffc5576d7c8ea05dcb590744958b275c16e
flotux <flvitalli@gmail.com>
2017-10-09 21:10:13 +0200

    add members and completed the __init__

--- Spring.py
+++ Spring.py
@@ -14,41 +14,37 @@ import sprlib
 class Spring(object):
 
     def __init__(self, ind, wire, diam, diam_tol, nb,
-                 machine, angle=0, angle_tol=0):
+                 material, angl=0, angl_tol=0):
         """
             ind -- indice of the spring
             wire -- wire diameter
             diam -- body diameter
             diam_tol -- diameter toleration
             nb -- number of coils
-            machine -- machine used
-            angle -OPTIONAL- angle of spring
-            angle_tol -OPTIONAL- angle toleration """
+            material -- the material used
+            angl -OPTIONAL- angle of spring
+            angl_tol -OPTIONAL- angle toleration """
 
         self.ind = ind
         self.wire = wire
         self.diam = diam
-        self.diam_tol = diam_tol
+        self.diam_tol = tol
         self.nb = nb
         self.machine = machine
+        self.material = material
 
+        ## if angle are find out ##
         if angle:
             self.angle = angle
             if angle_tol:
                 self.angle_tol = angle_tol
 
-
-    def evaluate(self):
-        """ Evaluate the spring and calcul all neccesary informations
-            with the sprlib library.
-            Called by init. """
-        pass
-
-
-    def save(self):
-        """ Saving spring in DTB """
-        pass
-
+        ## evaluation of the spring ##
+        self.sensor_tol = sprlib.sensor_tol(tol, nb)
+        self.mm_deg = sprlib.one_degree(wire, diam)
+        self.body_length = sprlib.body_length(wire, diam, nb)
+        self.body_weight = sprlib.body_weight(wire, nb)
+        self.diam_oob = sprlib.oob_diam(diam, material)
 
     def __str__(self):
         pass
