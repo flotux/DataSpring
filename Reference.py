b27f18d5a6575438247aaccb40b0fc32cc08811d
flotux <flvitalli@gmail.com>
2017-10-09 19:52:47 +0200

    add file

---
+++ Reference.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# Author : florian Vitalli
+# Contact : flvitalli@gmail.com
+
+"""
+    Reference.
+
+        It is the principal class in the programme.
+        Contain all information about a reference of spring :
+
+            - quotations of the spring
+            - machine used
+            - regulation of the machine
+            - tool used
+            - cadency
+
+        """
+
+import Spring
+import Machine
+
+class Reference(object):
+    pass
