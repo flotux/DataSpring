fe215c04b3c6468caff6a2e87abd0c456ebf3e10
unknown <flvitalli@gmail.com>
2018-01-07 18:22:14 +0100

    definition of spring class

--- view/popUp.py
+++ view/popUp.py
@@ -19,7 +19,7 @@
 
 # Labels on interface
 NEW_REGULATION = "Nouvelle fiche "
-MACHINE_SELECTOR_LABEL = "Machine utilisee "
+MACHINE_SELECTOR_LABEL = "Machine "
 WIRE_DIAMETER_LABEL = "Fil "
 
 from tkinter import *
@@ -42,6 +42,7 @@ class NewRegulationViewer(Tk):
         self.identity()
         self.machine()
         self.wire()
+        self.spring()
 
         self.quit_bton = Button(self, text="Quitter", command=self.destroy)
         self.quit_bton.grid(row=30, column=10, padx=2)
@@ -51,7 +52,8 @@ class NewRegulationViewer(Tk):
             and the references.
             """
 
-        Label(self, text="Reference Piece : ").grid(row=0, column=1, padx=10, pady=8)
+        Label(self, text="Reference Piece : ").grid(row=0, column=1, \
+                                                    padx=10, pady=8)
         self.id = Entry(self, width=20)
         self.id.grid(row=0, column=2)
         Label(self, text="Client : ").grid(row=0, column=3, padx=10, pady=8)
@@ -84,31 +86,79 @@ class NewRegulationViewer(Tk):
                                  increment=0.1, wrap=True, \
                                  command=self.wire_select)
         self.scaleWire.grid(row=1, column=2)
-        Label(self.fw, text="Diametre : ").grid(row=1, column=1, padx=10, pady=8)
+        Label(self.fw, text="Diametre : ").grid(row=1, column=1, \
+                                                padx=10, pady=8)
         Label(self.fw, text="mm").grid(row=1, column=3)
         # Wire references
-        Label(self.fw, text="Reference : ").grid(row=2, column=1, padx=10, pady=8)
+        Label(self.fw, text="Reference : ").grid(row=2, column=1, \
+                                                 padx=10, pady=8)
         self.entryWireRef = Entry(self.fw, width=10)
         self.entryWireRef.grid(row=2, column=2)
         # Wire material
-        self.wire_material = StringVar()
-        self.wire_material.set("Acier")
+        self.material = StringVar()
+        self.material.set("Acier")
         Label(self.fw, text="Matiere : ").grid(row=3, column=1, padx=10, pady=8)
-        self.radiogroup = Frame(self.fw)
-        self.radiogroup.grid(row=3, column=2)
-        self.steel = Radiobutton(self.radiogroup, text="Acier", indicatoron=0, \
-                                 variable=self.wire_material, value="steel", \
+        self.radiogroup_mat = Frame(self.fw)
+        self.radiogroup_mat.grid(row=3, column=2)
+        self.steel = Radiobutton(self.radiogroup_mat, text="Acier", indicatoron=0, \
+                                 variable=self.material, value="steel", \
                                  width=4)
         self.steel.grid(row=1, column=1)
         self.steel.deselect()
-        self.inox = Radiobutton(self.radiogroup, text="Inox", indicatoron=0, \
-                                variable=self.wire_material, value="inox", \
+        self.inox = Radiobutton(self.radiogroup_mat, text="Inox", indicatoron=0, \
+                                variable=self.material, value="inox", \
                                 width=4)
         self.inox.grid(row=1, column=2)
         self.inox.deselect()
 
     def spring(self):
-        pass
+
+        self.sep = ("_" * 80)
+        Label(self, text=self.sep).grid(row=2, padx=2, pady=10, columnspan=10)
+
+        self.fs = Frame(self)
+        self.fs.grid(row=3, column=0, pady=20, padx=10, columnspan=4)
+
+        self.dir = StringVar()
+        self.dir.set("Right")
+        Label(self.fs, text="sens d'enroulement : ").grid(row=1, column=1, \
+                                                          padx=5, pady=8)
+        self.radiogroup_dir = Frame(self.fs)
+        self.radiogroup_dir.grid(row=1, column=2)
+        self.right = Radiobutton(self.radiogroup_dir, text="Droite", indicatoron=0, \
+                                 variable=self.dir, value="right", \
+                                 width=8)
+        self.right.grid(row=1, column=2)
+        self.right.deselect()
+        self.left = Radiobutton(self.radiogroup_dir, text="Gauche", indicatoron=0, \
+                                variable=self.dir, value="left", \
+                                width=8)
+        self.left.grid(row=1, column=3)
+        self.left.deselect()
+
+        Label(self.fs, text="Diametre exterieur : ").grid(row=2, column=1, \
+                                                         padx=5, pady=8)
+        self.diam = DoubleVar()
+        self.diam.set(10.00)
+        self.entryDiam = Entry(self.fs, width=10)
+        self.entryDiam.grid(row=2, column=2)
+        Label(self.fs, text="mm").grid(row=2, column=3)
+
+        Label(self.fs, text="Nombre de spire : ").grid(row=3, column=1, \
+                                                         padx=5, pady=8)
+        self.nb = DoubleVar()
+        self.nb.set(10.00)
+        self.entryNb = Entry(self.fs, width=10)
+        self.entryNb.grid(row=3, column=2)
+        Label(self.fs, text="spires").grid(row=3, column=3)
+
+        Label(self.fs, text="Tolerance diametre +/- : ").grid(row=4, column=1, \
+                                                         padx=5, pady=8)
+        self.tol = DoubleVar()
+        self.tol.set(0.10)
+        self.entryTol = Entry(self.fs, width=10)
+        self.entryTol.grid(row=4, column=2)
+        Label(self.fs, text="mm").grid(row=4, column=3)
 
     def tools(self):
         pass
