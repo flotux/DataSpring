7aa98fedaee2bb14a45aa829a3f0419c77862c1b
Florian Vitalli <flvitalli@gmail.com>
2018-01-08 11:23:51 +0100

    creation of the class Overview and his function overlay.

--- view/root.py
+++ view/root.py
@@ -16,9 +16,10 @@ APP_NAME = "Data springs"
         """
 
 from tkinter import *
+from tkinter.ttk import *
 from . import popUp
 
-class StartMenu(Tk):
+class Root(Tk):
     """ The menu at the start of the programme. """
 
     def __init__(self):
@@ -32,7 +33,7 @@ class StartMenu(Tk):
         self.menuFile = Menu(self.menuBar, tearoff=0)
 
         self.menuNew = Menu(self.menuFile, tearoff=0)
-        self.menuNew.add_command(label="Fiche de reglage", command=self.n_reg)
+        self.menuNew.add_command(label="Fiche de reglage", command=self.new_reg)
         self.menuNew.add_command(label="Feuille de ressort", command=None)
         self.menuNew.add_command(label="Feuille d'outillage", command=None)
         self.menuFile.add_cascade(label="Nouveau", menu=self.menuNew)
@@ -47,15 +48,75 @@ class StartMenu(Tk):
 
         self.config(menu=self.menuBar)
 
-    def n_reg(self):
+        self.path = OverView()
+        self.destroy()
+        self.path.mainloop()
+
+    def new_reg(self):
         """ Used too lauch a new regulation viewer """
 
         self.path = popUp.NewRegulationViewer()
         self.path.mainloop()
 
+
 class OverView(Tk):
-    """ The page how the principal informations have been print. """
-    pass
+
+    def __init__(self):
+        Tk.__init__(self)
+        self.overlay()
+
+    def overlay(self):
+        """ Overlay of the machine.
+            """
+
+        # width
+        canw = 220
+        canw2 = canw/2
+        blocw = 110
+        # height
+        canh = 220
+        canh2 = canh/2
+        bloch = 180
+
+        self.can = Canvas(self, width=canw, height=canh)
+        self.can.grid(row=1, column=1, columnspan=3, rowspan=3)
+
+        self.can.create_line(canw2, 0, canw2, canh, width=1.4)
+        self.can.create_line(0, canh2, canw, canh2, width=1.4)
+        self.can.create_line(0, 0, canw, canh, width=1.4)
+        self.can.create_line(canw, 0, 0, canh, width=1.4)
+        self.can.create_oval(canw2-20, canh2-20, canw2+20, canh/2+20)
+
+        self.bloc1 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc1.grid(row=0, column=2, padx=10, pady=10)
+        self.add1 = Button(self.bloc1, text="AJOUTER")
+        self.add1.pack()
+        self.bloc2 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc2.grid(row=0, column=5, padx=10, pady=10)
+        self.add2 = Button(self.bloc2, text="AJOUTER")
+        self.add2.pack()
+        self.bloc3 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc3.grid(row=2, column=5, padx=10, pady=10)
+        self.bloc4 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc4.grid(row=5, column=5, padx=10, pady=10)
+        self.bloc5 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc5.grid(row=5, column=2, padx=10, pady=10)
+        self.bloc6 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc6.grid(row=5, column=0, padx=10, pady=10)
+        self.bloc7 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc7.grid(row=2, column=0, padx=10, pady=10)
+        self.bloc8 = LabelFrame(self, width=blocw, height=bloch)
+        self.bloc8.grid(row=0, column=0, padx=10, pady=10)
+
+    def characteristic(self):
+        pass
+
+    def informations(self):
+        pass
+
+    def entry(self):
+        pass
+
 
 if __name__ == '__main__':
-    StartMenu().mainloop()
+    Root().mainloop()
